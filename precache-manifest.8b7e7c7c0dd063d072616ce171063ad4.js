cde39328f66a9d9f8e291ce7e2e8a5bd1ff0324a
fatima-ola <fatimaolasunkanmi01@gmail.com>
2020-06-19 14:25:21 +0100

    Updates

---
+++ precache-manifest.8b7e7c7c0dd063d072616ce171063ad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661d8d24fb0c8ed97ab78e70a24ad814",
+    "url": "/authorization-form/index.html"
+  },
+  {
+    "revision": "e24bd845879a407292c6",
+    "url": "/authorization-form/static/js/2.7c1e641e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/authorization-form/static/js/2.7c1e641e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2951b7c64a1891796b6",
+    "url": "/authorization-form/static/js/main.df7ad5b7.chunk.js"
+  },
+  {
+    "revision": "613025766b903f580a7a",
+    "url": "/authorization-form/static/js/runtime-main.b9f1183b.js"
+  }
+]);
